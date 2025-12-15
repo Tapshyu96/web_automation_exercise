73e0acd612371bc0633b87f01eb7cf22def3e85a
tapshyu <tapshyuganvir@gmail.com>
2025-12-15 11:47:55 +0530

    Add some changes in test_orders test case

--- test_cases/test_orders.py
+++ test_cases/test_orders.py
@@ -1,5 +1,8 @@
 import pytest
 
 class TestOrders:
-  def test_place_order_with_register_flow():
+
+
+  def test_place_order_with_register_flow(self):
     print("Test Orders")
+    print("Test Orders2")
